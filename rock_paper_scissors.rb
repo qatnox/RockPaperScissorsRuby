46ed2edbb6836b685c86dda42b66f7d9c843adb9
Pavlo Zimnytsia <paha20zimnica16@gmail.com>
2023-09-11 04:11:02 +0300

    uploaded

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Passing two args to the program
+if ARGV.length != 2
+  puts "One argument is missing!"
+else
+  a = ARGV[0].downcase
+  b = ARGV[1].downcase
+
+  if a == b
+    puts "Draw!"
+  elsif (a == "rock" && b == "scissors") ||
+        (a == "scissors" && b == "paper") ||
+        (a == "paper" && b == "rock")
+    puts "You won!"
+  else
+    puts "You lose!"
+  end
+end
